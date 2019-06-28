d0abf82ef9a3188da5c04260ab6e4bedcffc7bbc
buck <coderbuck@163.com>
2019-06-28 17:20:05 +0800

    androidx

--- app/src/main/java/me/buck/sketchtest/MainActivity.java
+++ app/src/main/java/me/buck/sketchtest/MainActivity.java
@@ -1,13 +1,64 @@
 package me.buck.sketchtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import me.panpf.sketch.Sketch;
+import me.panpf.sketch.SketchImageView;
+import me.panpf.sketch.zoom.ImageZoomer;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @BindView(R.id.next)   Button          mNext;
+    @BindView(R.id.rotate) Button          mRotate;
+    @BindView(R.id.image)  SketchImageView mImage;
+
+    int[] imgList = {
+            R.drawable.img_1,
+            R.drawable.img_2,
+            R.drawable.img_3
+    };
+
+    private int curDisplayImgIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mImage.setZoomEnabled(true);
+        mImage.getOptions().setDecodeGifImage(true);
+        Sketch.with(this).displayFromResource(imgList[0], mImage)
+                .decodeGifImage()
+                .commit();
+    }
+
+    @OnClick(R.id.next)
+    public void onNextClicked() {
+        curDisplayImgIndex++;
+        curDisplayImgIndex = curDisplayImgIndex % 3;
+        Sketch.with(this).displayFromResource(imgList[curDisplayImgIndex], mImage)
+                .decodeGifImage()
+                .commit();
+    }
+
+    @OnClick(R.id.rotate)
+    public void onRotateClicked() {
+        ImageZoomer zoomer = mImage.getZoomer();
+        if (zoomer != null) {
+            float scale = zoomer.getZoomScale();
+            Log.d(TAG, "onRotateClicked: scale = " + scale);
+            zoomer.setZoomDuration(500);
+            zoomer.rotateBy(90);
+            zoomer.zoom(scale);
+        }
     }
 }
